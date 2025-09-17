ae2738dba1ffcaeb12f0d3e48b1a3348bcc2c6a2
deon-gracias <deon.gracias@adityabirla.com>
2025-09-17 17:06:39 +0530

    updated dependencies and formatting

--- tools/query_rag.py
+++ tools/query_rag.py
@@ -9,7 +9,16 @@ from langchain_core.runnables.config import RunnableConfig
 
 
 @tool
-async def fetch_relevant_response(user_query: str, user_id: str, file_id_list : list, notes:str, is_financial_statement: str, statement_type: List[Optional[str]] = [], core_statements: Optional[str] = None, top_k : int = 5):
+async def fetch_relevant_response(
+    user_query: str,
+    user_id: str,
+    file_id_list : list,
+    is_financial_statement: Optional[str] = None,
+    notes: Optional[str] = None,
+    statement_type: Optional[List[str]] = None,
+    core_statements: Optional[str] = None,
+    top_k : int = 5
+):
     """
     Tool : fetch_relevant_chunks
     Fetches the chunks from the vector database for the relevant user query.
@@ -22,7 +31,6 @@ async def fetch_relevant_response(user_query: str, user_id: str, file_id_list : 
         user_id (str): A unique identifier for individual user.
         `statement_type` ('consolidated', 'standalone', 'both', None): Which statement types to select from
         `is_financial_statement` ('Yes' or 'No' or None): Including financial statements section or not
-        `notes` ('No' or None): Including Notes to the financial statements section or not
         `core_statements` ('No' or None): Including core statements of an annual report such as balance_sheet, cash_flow, etc or not
 
     Returns:
@@ -34,7 +42,15 @@ async def fetch_relevant_response(user_query: str, user_id: str, file_id_list : 
         # logging.info(f" from tool logging top k : {top_k}, thread id : {thread_id}, query id : {query_id}, file id list : {file_id_list}, user query : {user_query}")
 
         # Fetch the RAG response and chunks asynchronously
-        rag_response = await retrieve_chunks(user_query=user_query, file_id_list = file_id_list, top_k=top_k, notes=notes, is_financial_statement=is_financial_statement, statement_type=statement_type, core_statements=core_statements)
+        rag_response = await retrieve_chunks(
+            user_query=user_query,
+            file_id_list = file_id_list,
+            top_k=top_k,
+            notes=notes,
+            is_financial_statement=is_financial_statement,
+            statement_type=statement_type,
+            core_statements=core_statements
+        )
 
         logging.info(f"Fetch Relevant Response: {len(rag_response['chunks'])}")
 
